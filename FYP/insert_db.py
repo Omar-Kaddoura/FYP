49e11ac19c30231737af8f9c329b305314a8f8c8
Omar-Kaddoura <157686716+Omar-Kaddoura@users.noreply.github.com>
2024-05-01 02:40:07 +0300

    Add files via upload

---
+++ FYP/insert_db.py
@@ -0,0 +1,83 @@
+from pymongo import MongoClient, errors
+from encodings.anas_encodings import anas_encodings
+from encodings.rahaf_encodings import rahaf_encodings  
+from encodings.assaad_encodings import assaad_encodings 
+from encodings.atieh_encodings import atieh_encodings 
+from encodings.hamza_encodings import hamza_encodings 
+from encodings.kaddoura_encodings import kaddoura_encodings
+client = MongoClient('mongodb+srv://rar29:uiwU4u6ZHBRLryVb@cluster0.e85xuag.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+
+db = client['mydatabase']
+collection = db['users']
+
+
+
+rahaf_encodings_list = [encoding.tolist() for encoding in rahaf_encodings]
+anas_encodings_list = [encoding.tolist() for encoding in anas_encodings]
+assaad_encodings_list = [encoding.tolist() for encoding in assaad_encodings]
+atieh_encodings_list = [encoding.tolist() for encoding in atieh_encodings]
+hamza_encodings_list = [encoding.tolist() for encoding in hamza_encodings]
+kaddoura_encodings_list = [encoding.tolist() for encoding in kaddoura_encodings]
+
+
+
+
+
+
+users_documents = [
+    {
+        "name": "Rahaf",
+        "encodings": rahaf_encodings_list,  
+        "ID": "1",
+    },
+    {
+        "name": "Atieh",
+        "encodings": atieh_encodings_list, 
+        "ID": "2",
+    },
+    {
+        "name": "Hamza",
+        "encodings": hamza_encodings_list,  
+        "ID": "3",
+    },
+    {
+        "name": "Kaddoura",
+        "encodings": kaddoura_encodings_list,  
+        "ID": "4",
+    },
+    {
+        "name": "Hamza",
+        "encodings": hamza_encodings_list,  
+        "ID": "5",
+    },
+    {
+        "name": "Anas",
+        "encodings": anas_encodings_list,  
+        "ID": "6",
+    },
+    {
+        "name": "Assaad",
+        "encodings": assaad_encodings_list,  
+        "ID": "7",
+    },
+]
+
+try:
+    for user in users_documents:
+        result = collection.update_one(
+            {"ID": user["ID"]},  # Use the correct field for lookup
+            {"$set": user},
+            upsert=True
+        )
+        if result.upserted_id is not None:
+            print(f"Inserted user ID: {user['ID']}")
+        else:
+            print(f"Updated user ID: {user['ID']}")
+
+    print("All users and encodings updated/inserted successfully.")
+except errors.BulkWriteError as e:
+    print("Error updating/inserting:", e)
+    for error_detail in e.details['writeErrors']:
+        print(f"Error with user ID {error_detail['op']['ID']}: {error_detail['errmsg']}")
+finally:
+    client.close()
